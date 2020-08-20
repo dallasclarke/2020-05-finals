3956c0e34c3cb2513e82ed4f724701389a9d84de
Dallas Clarke <dallas.clarke@codeimmersives.com>
2020-08-20 10:11:32 -0700

    Final

--- main.js
+++ main.js
@@ -1,9 +1,77 @@
 
+const makeRunOn = function(str) {
+  let result = "";
+  // result = result + str.replace(/\./g, ',')
+  
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === '.' && i != str.length-1) {
+      result += ','
+    } else {
+      result += str[i]
+    }
+  }
+  return result;
+}
 
+const totalScore = function(level, multiplier, bonus) {
+  let score = 0;
 
+  for (const levels of level) {
+    score += levels.score;
+  } if(multiplier) {
+    score *= multiplier;
+  } if (bonus) {
+    score += bonus;
+  }
+  return score;
+}
 
+const nightOwls = function(people) {
+  return people.filter((person) => {
+    if (person.asleep === false && person.localTime >= 100 && 400 >= person.localTime) {
+      return true;
+    }
+  })
+}
 
+const getToBed = function(people) {
+return people.filter((person) => {
+  if (person.asleep === false && person.localTime >= 100 && 400 >= person.localTime) {
+    return //????
+  }
+})
+}
 
+const findIndices = function(arr, func) {
+  let result = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if(func(arr[i])) {
+      result.push(i)
+    }
+  }
+  return result;
+
+}
+
+const Faqtory = function() {
+  return {
+    questions: [],
+    length: 0,
+    addQuestion: function(text) {
+    this.questions.push(
+      {text: text, id: this.length, answered: false}
+    )
+    this.length++
+  },
+  answerQuestion: function(id, answer) {
+    if(this.questions[id].answered === false) {
+      this.questions[id].answer = answer;
+      this.questions[id].answered = true;
+    }
+  }
+}
+}
 
 
 
